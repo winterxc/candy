9c13be4fb0f344d6cd8d62b401530f4ac2fcafe4
wangxiaocheng <w@zxianstudio.com>
2018-11-15 20:15:32 +0800

    add ext

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,9 @@
 				xxx
 			</gl-component>
 			<gl-component>
-				<golden-layout style="width:100%;height:100%;" :has-headers="true" :show-popout-icon="false">
+				<golden-layout style="width:100%;height:100%;" :has-headers="true" :show-popout-icon="false" :border-width="2">
 					<template slot="box" slot-scope="{i}">
-						<editor v-model="home" height="100%" preview-style="vertical" mode="wysiwyg" />
+						<editor v-model="home" :options="editorOptions" height="100%" preview-style="vertical" mode="wysiwyg" />
 					</template>
 					<gl-stack>
 						<gl-component v-for="i in [1,2,3,4]" :key="i" :title="'Test' + i" template="box" :state="{i}"/>
@@ -23,15 +23,23 @@ import 'golden-layout/src/css/goldenlayout-light-theme.css'
 import 'tui-editor/dist/tui-editor.css';
 import 'tui-editor/dist/tui-editor-contents.css';
 import 'codemirror/lib/codemirror.css';
-import { Editor } from '@toast-ui/vue-editor'
+import { Editor , Viewer} from '@toast-ui/vue-editor'
+import 'tui-editor/dist/tui-editor-extChart.js';//chart
+import 'tui-editor/dist/tui-editor-extScrollSync.js';//scrollSync
+import 'tui-editor/dist/tui-editor-extUML.js';//uml
+import 'tui-editor/dist/tui-editor-extColorSyntax.js';//colorSyntac
+import 'tui-editor/dist/tui-editor-extTable.js';//table
 import vgl from 'vue-golden-layout'
 Vue.use(vgl);
 
 export default {
-	components: {editor: Editor},
+	components: {editor: Editor, viewer: Viewer},
 	data: function(){
 		return {
-			home: 'Home'
+			home: "Home",
+			editorOptions: {
+				exts: ['chart', 'uml', 'scrollSync', 'colorSyntac', 'table']
+			}
 		}
 	}
 }
